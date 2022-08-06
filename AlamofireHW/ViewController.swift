45e54579c0f1a6629a16e6a155acf2b00b2f628f
VitalyTaran <79220120+VitalyTaran@users.noreply.github.com>
2022-08-07 00:47:57 +0300

    Initial Commit

---
+++ AlamofireHW/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlamofireHW
+//
+//  Created by Виталий Таран on 07.08.2022.
+//
+
+import UIKit
+import Pods_AlamofireHW
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
